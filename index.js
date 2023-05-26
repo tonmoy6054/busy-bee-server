fb02f01cef03dcb0214cee81b8786c07e04c48f5
Tonmoy Ghosh <tonmoy.mec@gmail.com>
2023-05-26 17:22:54 +0600

    initialized

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
-require('dotenv').config();
+
 
 // middleware
 app.use(cors());
 app.use(express.json());
 console.log(process.env.DB_PASS);
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o01z7ei.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -28,31 +27,31 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-
-const productCollection = client.db('busyBee').collection('products');
-
-
-app.get('/products', async(req, res)=>{
-  const cursor = productCollection.find();
-  const result = await cursor.toArray();
-  res.send(result);
-} )
-
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    // await client.close();
+    await client.close();
   }
 }
 run().catch(console.dir);
 
 
 
+
+
+
+
+
+
+
+
 app.get('/', (req, res) => {
-    res.send('bees are running')
+  res.send('bees are running')
 })
+
+
 
 app.listen(port, () => {
     console.log(`Busy Bee Toys Server is running on port ${port}`)
